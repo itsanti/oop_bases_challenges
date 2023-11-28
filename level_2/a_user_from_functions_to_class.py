8775223cef109a4ab69b71c1d693a772a326a3ef
Aleksandr <kurov.aleksandr@gmail.com>
2023-11-28 21:09:12 +0300

    Implement User dataclass

--- level_2/a_user_from_functions_to_class.py
+++ level_2/a_user_from_functions_to_class.py
@@ -4,15 +4,22 @@
 Задания:
     1. Создайте класс User и перенесите всю логику работы с пользователем туда.
 """
+from dataclasses import dataclass
 
 
-def make_username_capitalized(username: str):
-    return username.capitalize()
-
-
-def generate_short_user_description(username: str, user_id: int, name: str):
-    return f'User with id {user_id} has {username} username and {name} name'
-
-
+@dataclass(kw_only=True, frozen=True, slots=True)
 class User:
-    pass  # код писать тут
+    user_id: int
+    username: str
+    name: str
+
+    def make_username_capitalized(self):
+        return self.username.capitalize()
+
+    def generate_short_user_description(self):
+        return f'User with id {self.user_id} has {self.username} username and {self.name} name'
+
+
+user = User(user_id=1, username='admin', name='Aleksandr')
+print(user.make_username_capitalized())
+print(user.generate_short_user_description())
